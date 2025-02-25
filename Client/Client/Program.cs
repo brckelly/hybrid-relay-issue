977c54d2ebe45480ae62b6ba22bdd1c69cd1a473
Bryan Kelly <bkelly@symplr.com>
2025-02-25 16:08:33 -0600

    add sample apps

---
+++ Client/Client/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Client
+{
+	using Microsoft.Azure.Relay;
+	using System;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
+	using System.Threading.Tasks;
+	using System.Net.Http.Json;
+	using System.Net;
+	using System.Net.Http;
+
+	internal class Program
+    {
+		public static async Task Main(string[] args)
+		{
+			Console.WriteLine(".NET Version: " + Environment.Version);
+			var hostBuilder = Host.CreateDefaultBuilder();
+			hostBuilder.ConfigureServices(services =>
+			{
+				services.AddHttpClient("hybrid");
+			});
+
+			var host = hostBuilder.Build();
+
+			var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+			var keyName = configuration["keyName"];
+			var key = configuration["key"];
+			var relayNamespace = configuration["relayNamespace"];
+			var connectionName = configuration["connectionName"];
+			var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(keyName, key);
+
+			var address = $"https://{relayNamespace}/{connectionName}/";
+			var token = await tokenProvider.GetTokenAsync(address, TimeSpan.FromMinutes(20));
+
+			var httpClientFactory = host.Services.GetRequiredService<IHttpClientFactory>();
+			var client = httpClientFactory.CreateClient("hybrid");
+			client.BaseAddress = new Uri(address);
+
+			client.DefaultRequestHeaders.Add("ServiceBusAuthorization", token.TokenString);
+
+			try
+			{
+				var response = await client.PostAsync("test", JsonContent.Create("testing"));
+				var str = await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+				Console.WriteLine(str);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+    }
+}
---
+++ Listener/Listener/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Listener
+{
+	using Microsoft.Azure.Relay;
+	using System;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
+	using System.Threading.Tasks;
+	using System.Net.Http.Json;
+
+	internal class Program
+    {
+		public static async Task Main(string[] args)
+		{
+			Console.WriteLine(".NET Version: " + Environment.Version);
+			var hostBuilder = Host.CreateDefaultBuilder();
+			var host = hostBuilder.Build();
+
+			var configuration = host.Services.GetRequiredService<IConfiguration>();
+
+			var keyName = configuration["keyName"];
+			var key = configuration["key"];
+			var relayNamespace = configuration["relayNamespace"];
+			var connectionName = configuration["connectionName"];
+			var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(keyName, key);
+			var listener = new HybridConnectionListener(new Uri(string.Format("sb://{0}/{1}", relayNamespace, connectionName)), tokenProvider);
+
+			// Subscribe to the status events.
+			listener.Connecting += (o, e) => { Console.WriteLine("Connecting"); };
+			listener.Offline += (o, e) => { Console.WriteLine("Offline"); };
+			listener.Online += (o, e) => { Console.WriteLine("Online"); };
+
+			listener.RequestHandler += HandleConnection;
+
+			// Opening the listener establishes the control channel to
+			// the Azure Relay service. The control channel is continuously 
+			// maintained, and is reestablished when connectivity is disrupted.
+			await listener.OpenAsync();
+			Console.WriteLine("Server listening");
+
+			await host.RunAsync();
+		}
+
+		private static async void HandleConnection(RelayedHttpListenerContext context)
+		{
+			Console.WriteLine("Handling request...");
+
+			var obj = new
+			{
+				token = "abunchofjunk",
+				timeout = TimeSpan.FromMinutes(10)
+			};
+			var content = JsonContent.Create(obj);
+
+			context.Response.StatusCode = System.Net.HttpStatusCode.OK;
+			await content.CopyToAsync(context.Response.OutputStream);
+			await context.Response.CloseAsync();
+		}
+	}
+}
